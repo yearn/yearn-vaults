8cdc6e5794875aa9ee9ff357ef48c90715ee40e9
Shade Undertree <72354637+ShadeUndertree@users.noreply.github.com>
2020-10-09 15:46:59 -0700

    test: add xdist support

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+# Function scoped isolation fixture to enable xdist.
+# Snapshots the chain before each test and reverts after test completion.
+@pytest.fixture(scope="function", autouse=True)
+def shared_setup(fn_isolation):
+    pass
