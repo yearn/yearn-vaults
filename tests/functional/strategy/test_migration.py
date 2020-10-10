f79bdc87cd9ec0ee16cf7a43f355478a8865e4dc
Bryant <fubuloubu@gmail.com>
2020-10-09 22:09:24 -0400

    test: added a couple more conditions for coverage of authorities

--- tests/functional/strategy/test_migration.py
+++ tests/functional/strategy/test_migration.py
@@ -29,11 +29,22 @@ def test_good_migration(
     assert vault.strategies(new_strategy)[4] == strategy_debt
     assert token.balanceOf(new_strategy) == prior_position
 
+    # Also, governance can migrate directly
+    new_strategy.migrate(strategy, {"from": gov})
 
-def test_bad_migration(token, vault, strategy, gov, strategist, TestStrategy, Vault):
+
+def test_bad_migration(
+    token, vault, strategy, gov, strategist, TestStrategy, Vault, rando
+):
     different_vault = gov.deploy(Vault, token, gov, gov)
     new_strategy = strategist.deploy(TestStrategy, different_vault, gov)
 
     # Can't migrate to a strategy with a different vault
     with brownie.reverts():
         vault.migrateStrategy(strategy, new_strategy, {"from": gov})
+
+    new_strategy = strategist.deploy(TestStrategy, vault, gov)
+
+    # Can't migrate if you're not the Vault  or governance
+    with brownie.reverts():
+        strategy.migrate(new_strategy, {"from": rando})
--- tests/functional/strategy/test_withdrawal.py
+++ tests/functional/strategy/test_withdrawal.py
@@ -1,4 +1,7 @@
-def test_withdraw(gov, token, vault, strategy):
+import brownie
+
+
+def test_withdraw(gov, token, vault, strategy, rando):
     token.approve(vault, token.balanceOf(gov), {"from": gov})
     vault.deposit(token.balanceOf(gov) // 2, {"from": gov})
     strategy.harvest({"from": gov})  # Seed some debt in there
@@ -7,6 +10,10 @@ def test_withdraw(gov, token, vault, strategy):
     balance = strategy.estimatedTotalAssets()
     strategy.withdraw(balance // 2, {"from": vault.address})
     assert strategy.estimatedTotalAssets() == balance // 2
+
+    # Not just anyone can call it
+    with brownie.reverts():
+        strategy.withdraw(balance // 2, {"from": rando})
 
     # Anything over what we can liquidate is totally withdrawn
     strategy.withdraw(balance, {"from": vault.address})
