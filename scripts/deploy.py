a7c52b5bff56243f87083ac26f31af00ab930c73
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2020-10-20 16:57:09 -0400

    feat: upgrade ux for deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,21 +1,49 @@
-from brownie import Vault, accounts, network
+from pathlib import Path
+import yaml
+
+from brownie import Token, Vault, accounts, network, web3
 from eth_utils import is_checksum_address
+
+PACKAGE_VERSION = yaml.safe_load(
+    (Path(__file__).parent.parent / "ethpm-config.yaml").read_text()
+)["version"]
 
 
 def get_address(msg: str) -> str:
     while True:
-        addr = input(msg)
-        if is_checksum_address(addr):
-            return addr
-        print(f"I'm sorry, but '{addr}' is not a checksummed address")
+        val = input(msg)
+        if is_checksum_address(val):
+            return val
+        else:
+            addr = web3.ens.address(val)
+            if addr:
+                print(f"Found ENS '{val}' [{addr}]")
+                return addr
+        print(f"I'm sorry, but '{val}' is not a checksummed address or ENS")
 
 
 def main():
     print(f"You are using the '{network.show_active()}' network")
     dev = accounts.load("dev")
     print(f"You are using: 'dev' [{dev.address}]")
-    token = get_address("ERC20 Token: ")
+    token = Token.at(get_address("ERC20 Token: "))
     gov = get_address("yEarn Governance: ")
     rewards = get_address("Rewards contract: ")
+    name = input(f"Set description ['yearn {token.name()}']: ") or ""
+    symbol = input(f"Set symbol ['y{token.symbol()}']: ") or ""
+    print(
+        f"""
+    Vault Parameters
+
+   version: {PACKAGE_VERSION}
+     token: {token.address}
+  governer: {gov}
+   rewards: {rewards}
+      name: '{name or 'yearn ' + token.name()}'
+    symbol: '{symbol or 'y' + token.symbol()}'
+    """
+    )
+    if input("Deploy New Vault? y/[N]: ").lower() != "y":
+        return
     print("Deploying Vault...")
-    vault = dev.deploy(Vault, token, gov, rewards, "", "")
+    vault = dev.deploy(Vault, token, gov, rewards, name, symbol)
