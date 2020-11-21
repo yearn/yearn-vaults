38b98b2f02048611fef5bfd4156365181822e161
Macarse <macarse@gmail.com>
2020-11-21 09:45:58 -0600

    fix: fix flakiness in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,5 @@ import pytest
 # Function scoped isolation fixture to enable xdist.
 # Snapshots the chain before each test and reverts after test completion.
 @pytest.fixture(scope="function", autouse=True)
-def shared_setup(fn_isolation):
+def isolation(fn_isolation):
     pass
