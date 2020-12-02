cdc55ef466bb11d4f041f7794249a31f73d4c7a1
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2020-12-02 14:51:43 -0500

    refactor: upgrade keeper script (#92)
    * feat: upgrade keeper script for gas strategy, gas cost estimates, etc
    * chore: upgrade to brownie v1.12.0
    * chore: upgrade to Brownie v1.12.1 for keeper script
    * fix: forgot f in f-string

--- scripts/keep.py
+++ scripts/keep.py
@@ -1,8 +1,14 @@
-from brownie import accounts, network, interface, Vault
+from brownie import accounts, network, interface, Vault, Token
+from brownie.network.gas.strategies import GasNowScalingStrategy
 from decimal import Decimal
 from eth_utils import is_checksum_address
 import requests
 from time import sleep
+
+
+GAS_BUFFER = 1.2
+
+gas_strategy = GasNowScalingStrategy()
 
 
 def get_address(msg: str) -> str:
@@ -11,13 +17,6 @@ def get_address(msg: str) -> str:
         if is_checksum_address(addr):
             return addr
         print(f"I'm sorry, but '{addr}' is not a checksummed address")
-
-
-def get_gas_price(confirmation_speed: str = "fast"):
-    if "mainnet" not in network.show_active():
-        return 10 ** 9  # 1 gwei
-    data = requests.get("https://www.gasnow.org/api/v3/gas/price").json()
-    return data["data"][confirmation_speed]
 
 
 def main():
@@ -30,45 +29,80 @@ def main():
         strategies.append(interface.StrategyAPI(get_address("Strategy to farm: ")))
 
     vault = Vault.at(strategies[0].vault())
+    want = Token.at(vault.token())
 
     for strategy in strategies:
         assert (
             strategy.keeper() == bot.address
         ), "Bot is not set as keeper! [{strategy.address}]"
-        assert strategy.vault() == vault.address, "Vault mismatch! [{strategy.address}]"
+        assert (
+            strategy.vault() == vault.address
+        ), f"Vault mismatch! [{strategy.address}]"
 
     while True:
-        gas_price = get_gas_price()
         starting_balance = bot.balance()
 
-        no_action = True
+        calls_made = 0
+        total_gas_estimate = 0
         for strategy in strategies:
             # Display some relevant statistics
-            symbol = vault.symbol()[1:]
+            symbol = want.symbol()
             credit = vault.creditAvailable(strategy) / 10 ** vault.decimals()
             print(f"[{strategy.address}] Credit Available: {credit:0.3f} {symbol}")
             debt = vault.debtOutstanding(strategy) / 10 ** vault.decimals()
             print(f"[{strategy.address}] Debt Outstanding: {debt:0.3f} {symbol}")
 
-            # TODO: Actually estimate gas
-            if strategy.tendTrigger(40000 * gas_price):
-                try:
-                    strategy.tend({"from": bot, "gas_price": gas_price})
-                    no_action = False
-                except:
-                    print("Call failed")
+            starting_gas_price = next(gas_strategy.get_gas_price())
 
-            elif strategy.harvestTrigger(180000 * gas_price):
-                try:
-                    strategy.harvest({"from": bot, "gas_price": gas_price})
-                    no_action = False
-                except:
-                    print("Call failed")
+            try:
+                tend_gas_estimate = int(
+                    GAS_BUFFER * strategy.tend.estimate_gas({"from": bot})
+                )
+                total_gas_estimate += tend_gas_estimate
+            except ValueError:
+                print(f"[{strategy.address}] `tend` estimate fails")
+                tend_gas_estimate = None
 
-        if no_action:
+            try:
+                harvest_gas_estimate = int(
+                    GAS_BUFFER * strategy.harvest.estimate_gas({"from": bot})
+                )
+                total_gas_estimate += harvest_gas_estimate
+            except ValueError:
+                print(f"[{strategy.address}] `harvest` estimate fails")
+                harvest_gas_estimate = None
+
+            if harvest_gas_estimate and strategy.harvestTrigger(
+                harvest_gas_estimate * starting_gas_price
+            ):
+                try:
+                    strategy.harvest({"from": bot, "gas_price": gas_strategy})
+                    calls_made += 1
+                except:
+                    print(f"[{strategy.address}] `harvest` call fails")
+
+            elif tend_gas_estimate and strategy.tendTrigger(
+                tend_gas_estimate * starting_gas_price
+            ):
+                try:
+                    strategy.tend({"from": bot, "gas_price": gas_strategy})
+                    calls_made += 1
+                except:
+                    print(f"[{strategy.address}] `tend` call fails")
+
+        # Check running 10 `tend`s & `harvest`s per strategy at estimated gas price
+        # would empty the balance of the bot account
+        if bot.balance() < 10 * total_gas_estimate * starting_gas_price:
+            print(f"Need more ether please! {bot.address}")
+
+        # Wait a minute if we didn't make any calls
+        if calls_made > 0:
+            gas_cost = (starting_balance - bot.balance()) / 10 ** 18
+            num_harvests = bot.balance() // (starting_balance - bot.balance())
+            print(f"Made {calls_made} calls, spent {gas_cost} ETH on gas.")
+            print(
+                f"At this rate, it'll take {num_harvests} harvests to run out of gas."
+            )
+        else:
             print("Sleeping for 60 seconds...")
             sleep(60)
-
-        if bot.balance() < 10 ** 8 * gas_price:
-            # Less than 100m gas left to spend
-            print(f"Need more ether please! {bot.address}")
