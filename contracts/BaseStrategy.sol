521fff1586cb1becc79fd95d85f64018e9eebb9a
aliatiia <BtKwBiAt@pm.me>
2020-12-08 01:45:21 -0500

    fix: check for success

--- contracts/BaseStrategy.sol
+++ contracts/BaseStrategy.sol
@@ -616,6 +616,7 @@ abstract contract BaseStrategy {
     function migrate(address _newStrategy) external {
         require(msg.sender == address(vault) || msg.sender == governance());
         require(BaseStrategy(_newStrategy).vault() == vault);
+        // Explicitly require a migration success signal from implementers of `prepareMigration`
         require(prepareMigration(_newStrategy));
         want.transfer(_newStrategy, want.balanceOf(address(this)));
     }
