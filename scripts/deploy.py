1ebef3512ebebf9d575242c268be98ed37c00916
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2020-12-21 15:59:18 -0500

    refactor: use click more extensively

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,36 +6,47 @@ from brownie import Token, Vault, accounts, network, web3
 from eth_utils import is_checksum_address
 
 
+DEFAULT_VAULT_NAME = lambda token: f"{token.symbol()} yVault"
+DEFAULT_VAULT_SYMBOL = lambda token: f"yv{token.symbol()}"
+
 PACKAGE_VERSION = yaml.safe_load(
     (Path(__file__).parent.parent / "ethpm-config.yaml").read_text()
 )["version"]
 
 
-def get_address(msg: str) -> str:
+def get_address(msg: str, default: str = None) -> str:
+    val = click.prompt(msg, default=default)
+
+    # Keep asking user for click.prompt until it passes
     while True:
-        val = input(msg)
+
         if is_checksum_address(val):
             return val
-        else:
-            addr = web3.ens.address(val)
-            if addr:
-                print(f"Found ENS '{val}' [{addr}]")
-                return addr
-        print(f"I'm sorry, but '{val}' is not a checksummed address or ENS")
+        elif addr := web3.ens.address(val):
+            click.echo(f"Found ENS '{val}' [{addr}]")
+            return addr
+
+        click.echo(
+            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
+        )
+        # NOTE: Only display default once
+        val = click.prompt(msg)
 
 
 def main():
-    print(f"You are using the '{network.show_active()}' network")
+    click.echo(f"You are using the '{network.show_active()}' network")
     dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
-    print(f"You are using: 'dev' [{dev.address}]")
-    token = Token.at(get_address("ERC20 Token: "))
-    gov = get_address("Yearn Governance [ychad.eth]: ")
+    click.echo(f"You are using: 'dev' [{dev.address}]")
+
+    token = Token.at(get_address("ERC20 Token"))
+    gov = get_address("Yearn Governance", default="ychad.eth")
     rewards = get_address(
-        "Rewards contract [0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde]: "
+        "Rewards contract", default="0x93A62dA5a14C80f265DAbC077fCEE437B1a0Efde"
     )
-    name = input(f"Set description ['{token.name()} yVault']: ") or ""
-    symbol = input(f"Set symbol ['yv{token.symbol()}']: ") or ""
-    print(
+    name = click.prompt(f"Set description", default=DEFAULT_VAULT_NAME(token))
+    symbol = click.prompt(f"Set symbol", default=DEFAULT_VAULT_SYMBOL(token))
+
+    click.echo(
         f"""
     Vault Parameters
 
@@ -43,11 +54,18 @@ def main():
      token: {token.address}
   governer: {gov}
    rewards: {rewards}
-      name: '{name or 'yearn ' + token.name()}'
-    symbol: '{symbol or 'y' + token.symbol()}'
+      name: '{name}'
+    symbol: '{symbol}'
     """
     )
-    if input("Deploy New Vault? y/[N]: ").lower() != "y":
-        return
-    print("Deploying Vault...")
-    vault = dev.deploy(Vault, token, gov, rewards, name, symbol)
+
+    if click.confirm("Deploy New Vault"):
+        vault = dev.deploy(
+            Vault,
+            token,
+            gov,
+            rewards,
+            # NOTE: Empty string `""` means no override (don't use click default tho)
+            name if name != DEFAULT_VAULT_NAME(token) else "",
+            symbol if symbol != DEFAULT_VAULT_SYMBOL(token) else "",
+        )
