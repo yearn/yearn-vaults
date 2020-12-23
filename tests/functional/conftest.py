2ab39736cfed31032c27f831b2a6b8722a7b7619
Macarse <macarse@gmail.com>
2020-12-23 17:13:17 -0600

    fix: pr review2

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -27,8 +27,9 @@ def token(gov, Token):
 
 
 @pytest.fixture
-def vault(gov, guardian, token, rewards, Vault):
+def vault(gov, guardian, management, token, rewards, Vault):
     vault = guardian.deploy(Vault, token, gov, rewards, "", "")
+    vault.setManagement(management, {"from": gov})
     # Make it so vault has some AUM to start
     token.approve(vault, token.balanceOf(gov) // 2, {"from": gov})
     vault.deposit(token.balanceOf(gov) // 2, {"from": gov})
--- tests/functional/vault/test_config.py
+++ tests/functional/vault/test_config.py
@@ -9,6 +9,8 @@ PACKAGE_VERSION = yaml.safe_load(
     (Path(__file__).parent.parent.parent.parent / "ethpm-config.yaml").read_text()
 )["version"]
 
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
 
 def test_vault_deployment(guardian, gov, rewards, token, Vault):
     # Deploy the Vault without any name/symbol overrides
@@ -43,30 +45,172 @@ def test_vault_name_symbol_override(guardian, gov, rewards, token, Vault):
 
 
 @pytest.mark.parametrize(
-    "getter,setter,val",
+    "getter,setter,val,gov_allowed,mgmt_allowed,guard_allowed",
     [
-        ("name", "setName", "NewName yVault"),
-        ("symbol", "setSymbol", "yvNEW"),
-        ("emergencyShutdown", "setEmergencyShutdown", True),
-        ("guardian", "setGuardian", None),
-        ("rewards", "setRewards", None),
-        ("management", "setManagement", None),
-        ("performanceFee", "setPerformanceFee", 1000),
-        ("managementFee", "setManagementFee", 1000),
-        ("depositLimit", "setDepositLimit", 1000),
+        ("name", "setName", "NewName yVault", True, False, False),
+        ("symbol", "setSymbol", "yvNEW", True, False, False),
+        ("emergencyShutdown", "setEmergencyShutdown", True, True, False, True),
+        ("guardian", "setGuardian", None, True, False, True),
+        ("rewards", "setRewards", None, True, False, False),
+        ("management", "setManagement", None, True, False, False),
+        ("performanceFee", "setPerformanceFee", 1000, True, False, False),
+        ("managementFee", "setManagementFee", 1000, True, False, False),
+        ("depositLimit", "setDepositLimit", 1000, True, False, False),
     ],
 )
-def test_vault_setParams(gov, vault, rando, getter, setter, val):
+def test_vault_setParams(
+    chain,
+    gov,
+    guardian,
+    management,
+    vault,
+    rando,
+    getter,
+    setter,
+    val,
+    gov_allowed,
+    mgmt_allowed,
+    guard_allowed,
+):
     if not val:
         # Can't access fixtures, so use None to mean any random address
         val = rando
 
-    # Only governance can set this param
+    # rando shouldn't be able to call these methods
     with brownie.reverts():
         getattr(vault, setter)(val, {"from": rando})
 
+    if guard_allowed:
+        getattr(vault, setter)(val, {"from": guardian})
+        assert getattr(vault, getter)() == val
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(val, {"from": guardian})
+
+    if mgmt_allowed:
+        getattr(vault, setter)(val, {"from": management})
+        assert getattr(vault, getter)() == val
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(val, {"from": management})
+
     getattr(vault, setter)(val, {"from": gov})
     assert getattr(vault, getter)() == val
+
+
+@pytest.mark.parametrize(
+    "key,setter,val,gov_allowed,mgmt_allowed,guard_allowed",
+    [
+        ("debtLimit", "updateStrategyDebtLimit", 500, True, True, False),
+        ("rateLimit", "updateStrategyRateLimit", 10, True, True, False),
+    ],
+)
+def test_vault_updateStrategy(
+    chain,
+    gov,
+    guardian,
+    management,
+    vault,
+    strategy,
+    rando,
+    key,
+    setter,
+    val,
+    gov_allowed,
+    mgmt_allowed,
+    guard_allowed,
+):
+
+    # rando shouldn't be able to call these methods
+    with brownie.reverts():
+        getattr(vault, setter)(strategy, val, {"from": rando})
+
+    if guard_allowed:
+        getattr(vault, setter)(strategy, val, {"from": guardian})
+        assert vault.strategies(strategy).dict()[key] == val
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(strategy, val, {"from": guardian})
+
+    if mgmt_allowed:
+        getattr(vault, setter)(strategy, val, {"from": management})
+        assert vault.strategies(strategy).dict()[key] == val
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(strategy, val, {"from": management})
+
+    getattr(vault, setter)(strategy, val, {"from": gov})
+    assert vault.strategies(strategy).dict()[key] == val
+
+
+@pytest.mark.parametrize(
+    "count,setter,gov_allowed,mgmt_allowed,guard_allowed",
+    [
+        (20, "setWithdrawalQueue", True, True, False),
+        (0, "removeStrategyFromQueue", True, True, False),
+        (1, "addStrategyToQueue", True, True, False),
+    ],
+)
+def test_vault_withdrawalQueue(
+    chain,
+    gov,
+    guardian,
+    management,
+    vault,
+    strategy,
+    rando,
+    count,
+    setter,
+    gov_allowed,
+    mgmt_allowed,
+    guard_allowed,
+):
+    val = None
+    if setter == "removeStrategyFromQueue":
+        val = vault.withdrawalQueue(0)
+    elif setter == "addStrategyToQueue":
+        val = vault.withdrawalQueue(0)
+        vault.removeStrategyFromQueue(val, {"from": gov})
+    elif setter == "setWithdrawalQueue":
+        val = [vault.withdrawalQueue(0)] * 20
+
+    # rando shouldn't be able to call these methods
+    with brownie.reverts():
+        getattr(vault, setter)(val, {"from": rando})
+
+    if guard_allowed:
+        getattr(vault, setter)(val, {"from": guardian})
+        assert strategies_len(vault) == count
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(val, {"from": guardian})
+
+    if mgmt_allowed:
+        getattr(vault, setter)(val, {"from": management})
+        assert strategies_len(vault) == count
+        chain.undo()
+    else:
+        with brownie.reverts():
+            getattr(vault, setter)(val, {"from": management})
+
+    getattr(vault, setter)(val, {"from": gov})
+    assert strategies_len(vault) == count
+
+
+def strategies_len(vault):
+    count = 0
+    for i in range(0, 20):
+        if vault.withdrawalQueue(i) == ZERO_ADDRESS:
+            break
+
+        count += 1
+
+    return count
 
 
 def test_vault_setGovernance(gov, vault, rando):
