613dffdd0d714b3797ae0fabbad96da5a7efcb19
Luciano Bertenasco <lbertenasco@gmail.com>
2020-12-28 12:08:54 -0300

    feat: removes openzeppelin V3

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "@openzeppelinV3/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor() public ERC20("yearn.finance test token", "TEST") {
