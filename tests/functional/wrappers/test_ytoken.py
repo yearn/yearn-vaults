e9f7012b6d73a195814ee867ce1d7ce764ce7dba
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-02-08 14:11:43 -0500

    test: add tests for yToken deposit/transfer/withdraw

--- tests/functional/wrappers/test_ytoken.py
+++ tests/functional/wrappers/test_ytoken.py
@@ -29,3 +29,42 @@ def test_config(gov, token, vault, registry, ytoken):
     assert ytoken.symbol() == vault.symbol()
     assert ytoken.decimals() == vault.decimals()
     assert ytoken.allVaults() == [vault]
+
+
+def test_deposit(token, registry, vault, ytoken, gov, rando):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    assert ytoken.balanceOf(rando) == vault.balanceOf(rando) == 0
+
+    # NOTE: Must approve ytoken to deposit
+    token.approve(ytoken, 10000, {"from": rando})
+    ytoken.deposit(10000, {"from": rando})
+    assert ytoken.balanceOf(rando) == vault.balanceOf(rando) == 10000
+
+
+def test_transfer(token, registry, vault, ytoken, gov, rando, affiliate):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    token.approve(ytoken, 10000, {"from": rando})
+    ytoken.deposit(10000, {"from": rando})
+
+    # NOTE: Must approve ytoken to withdraw and send
+    vault.approve(ytoken, 10000, {"from": rando})
+    # NOTE: Just using `affiliate` as a random address
+    ytoken.transfer(affiliate, 10000, {"from": rando})
+    assert ytoken.balanceOf(rando) == vault.balanceOf(rando) == 0
+    assert token.balanceOf(rando) == 0
+    assert token.balanceOf(affiliate) == 10000
+
+
+def test_withdraw(token, registry, vault, ytoken, gov, rando):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    token.approve(ytoken, 10000, {"from": rando})
+    ytoken.deposit(10000, {"from": rando})
+
+    # NOTE: Must approve ytoken to withdraw
+    vault.approve(ytoken, 10000, {"from": rando})
+    ytoken.withdraw(10000, {"from": rando})
+    assert ytoken.balanceOf(rando) == vault.balanceOf(rando) == 0
+    assert token.balanceOf(rando) == 10000
