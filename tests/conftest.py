af620c4cdb8b4f4d221d4f992829d3fbefbe571a
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-02-11 21:57:04 -0500

    refactor: move permit signature to it's own fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import pytest
 import yaml
 
+from eth_account import Account
+from eth_account.messages import encode_structured_data
+
 from brownie import compile_source, Vault
 
 PACKAGE_VERSION = yaml.safe_load(
@@ -79,6 +82,60 @@ def patch_vault_version():
             return compile_source(source).Vyper
 
     return patch_vault_version
+
+
+@pytest.fixture
+def sign_vault_permit():
+    def sign_vault_permit(
+        vault: Vault,
+        owner: Account,  # NOTE: Must be a eth_key account, not Brownie
+        spender: str,
+        allowance: int = 2 ** 256 - 1,  # Allowance to set with `permit`
+        deadline: int = 0,  # 0 means no time limit
+        override_nonce: int = None,
+    ):
+        name = "Yearn Vault"
+        version = vault.apiVersion()
+        chain_id = 1  # ganache bug https://github.com/trufflesuite/ganache/issues/1643
+        if override_nonce:
+            nonce = override_nonce
+        else:
+            nonce = vault.nonces(owner.address)
+        data = {
+            "types": {
+                "EIP712Domain": [
+                    {"name": "name", "type": "string"},
+                    {"name": "version", "type": "string"},
+                    {"name": "chainId", "type": "uint256"},
+                    {"name": "verifyingContract", "type": "address"},
+                ],
+                "Permit": [
+                    {"name": "owner", "type": "address"},
+                    {"name": "spender", "type": "address"},
+                    {"name": "value", "type": "uint256"},
+                    {"name": "nonce", "type": "uint256"},
+                    {"name": "deadline", "type": "uint256"},
+                ],
+            },
+            "domain": {
+                "name": name,
+                "version": version,
+                "chainId": chain_id,
+                "verifyingContract": str(vault),
+            },
+            "primaryType": "Permit",
+            "message": {
+                "owner": owner.address,
+                "spender": spender,
+                "value": allowance,
+                "nonce": nonce,
+                "deadline": deadline,
+            },
+        }
+        permit = encode_structured_data(data)
+        return owner.sign_message(permit).signature
+
+    return sign_vault_permit
 
 
 # Function scoped isolation fixture to enable xdist.
