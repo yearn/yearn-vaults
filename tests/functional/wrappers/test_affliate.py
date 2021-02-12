ac9cf1b33e5ba1d735d134e6cdf9670777c8020b
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-02-11 21:22:09 -0500

    test: add tests for affiliate token

--- tests/functional/wrappers/test_affliate.py
+++ tests/functional/wrappers/test_affliate.py
@@ -20,3 +20,40 @@ def test_config(gov, token, vault, registry, affiliate_token):
     registry.newRelease(vault, {"from": gov})
     assert affiliate_token.bestVault() == vault
     assert affiliate_token.allVaults() == [vault]
+
+
+def test_deposit(token, registry, vault, affiliate_token, gov, rando):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    assert affiliate_token.balanceOf(rando) == vault.balanceOf(rando) == 0
+
+    # NOTE: Must approve affiliate_token to deposit
+    token.approve(affiliate_token, 10000, {"from": rando})
+    affiliate_token.deposit(10000, {"from": rando})
+    assert affiliate_token.balanceOf(rando) == 10000
+    assert vault.balanceOf(rando) == 0
+
+
+def test_transfer(token, registry, vault, affiliate_token, gov, rando, affiliate):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    token.approve(affiliate_token, 10000, {"from": rando})
+    affiliate_token.deposit(10000, {"from": rando})
+
+    # NOTE: Just using `affiliate` as a random address
+    affiliate_token.transfer(affiliate, 10000, {"from": rando})
+    assert affiliate_token.balanceOf(rando) == 0
+    assert affiliate_token.balanceOf(affiliate) == 10000
+    assert token.balanceOf(rando) == token.balanceOf(affiliate) == 0
+
+
+def test_withdraw(token, registry, vault, affiliate_token, gov, rando):
+    registry.newRelease(vault, {"from": gov})
+    token.transfer(rando, 10000, {"from": gov})
+    token.approve(affiliate_token, 10000, {"from": rando})
+    affiliate_token.deposit(10000, {"from": rando})
+
+    # NOTE: Must approve affiliate_token to withdraw
+    affiliate_token.withdraw(10000, {"from": rando})
+    assert affiliate_token.balanceOf(rando) == 0
+    assert token.balanceOf(rando) == 10000
