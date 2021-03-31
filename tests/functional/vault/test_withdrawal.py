1e6f137a5459e19c8f67e785c67c96fc4912987c
dougstorm <dougstorm@users.noreply.github.com>
2021-03-31 12:00:57 -0600

    feat: add partial withdraw test with delegated assets

--- tests/functional/vault/test_withdrawal.py
+++ tests/functional/vault/test_withdrawal.py
@@ -326,7 +326,7 @@ def test_profit_degration(chain, gov, token, vault, strategy, rando):
     assert vault.pricePerShare() >= pricePerShareBefore * 2 * 0.99
 
 
-def test_withdraw_delegate(chain, gov, token, vault, strategy, rando):
+def test_withdraw_delegate_assets(chain, gov, token, vault, strategy, rando):
     # set fees to 0
     vault.setManagementFee(0, {"from": gov})
     vault.setPerformanceFee(0, {"from": gov})
@@ -356,8 +356,58 @@ def test_withdraw_delegate(chain, gov, token, vault, strategy, rando):
 
     vault.withdraw({"from": gov})
 
-    # check side effects on delegate assets accounting
+    assert strategy.delegatedAssets() == vault.strategies(strategy).dict()["totalDebt"]
+    delegated_assets_after = vault.delegatedAssets()
+    assert delegated_assets_after < delegated_assets_before
     assert vault.totalSupply() == 0
     assert token.balanceOf(vault) == 0  # everything is withdrawn
-    delegated_assets_after = vault.delegatedAssets()
     assert delegated_assets_after == 0
+
+
+def test_withdraw_partial_delegate_assets(chain, gov, token, vault, strategy, rando):
+    # set fees to 0
+    vault.setManagementFee(0, {"from": gov})
+    vault.setPerformanceFee(0, {"from": gov})
+    vault.updateStrategyPerformanceFee(strategy, 0, {"from": gov})
+
+    vault.setLockedProfitDegration(
+        1e18, {"from": gov}
+    )  # Set profit degradation to 1 sec.
+    deposit = vault.totalAssets()
+    pricePerShareBefore = vault.pricePerShare()
+    token.transfer(strategy, vault.totalAssets(), {"from": gov})  # seed some profit
+    strategy.harvest({"from": gov})
+
+    chain.sleep(1)
+    chain.mine(1)  # cant withdraw on same block
+
+    assert vault.pricePerShare() == pricePerShareBefore * 2  # profit
+    assert vault.totalAssets() == deposit * 2  # profit
+
+    # Check delegation math/logic
+    strategy._toggleDelegation()
+    assert strategy.delegatedAssets() == vault.strategies(strategy).dict()["totalDebt"]
+    assert vault.delegatedAssets() == 0  # NOTE: Cached 1 harvest period behind
+    strategy.harvest()
+    delegated_assets_before = vault.delegatedAssets()
+    strategy_delegated_assets_before = strategy.delegatedAssets()
+    assert delegated_assets_before == strategy_delegated_assets_before
+
+    # not all deposit is delegated
+    assert vault.balanceOf(gov) >= delegated_assets_before
+
+    # withdraw up to delegated assets a partial amount
+    vault.withdraw(delegated_assets_before, {"from": gov})
+
+    strategy_delegated_assets_after = strategy.delegatedAssets()
+    assert (
+        strategy_delegated_assets_after
+        == vault.strategies(strategy).dict()["totalDebt"]
+    )
+    new_delegated_assets_total = (
+        delegated_assets_before - strategy_delegated_assets_before
+    ) + strategy_delegated_assets_after
+    delegated_assets_after = vault.delegatedAssets()
+    assert delegated_assets_after < delegated_assets_before
+    # check amount is updated correctly after side effect
+    assert delegated_assets_after == new_delegated_assets_total
