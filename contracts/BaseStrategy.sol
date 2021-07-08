4dbda81a12788a055a41549b4150d0aa9094e28c
stcrab <2143646+stcrab@users.noreply.github.com>
2021-07-08 19:35:21 +0200

    chore: bump version (#438)

--- contracts/BaseStrategy.sol
+++ contracts/BaseStrategy.sol
@@ -203,7 +203,7 @@ abstract contract BaseStrategy {
      * @return A string which holds the current API version of this contract.
      */
     function apiVersion() public pure returns (string memory) {
-        return "0.4.2";
+        return "0.4.3";
     }
 
     /**
