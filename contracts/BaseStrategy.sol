c09741cdd1d9a3e6f7274664cbe0afc07e108119
panda <87183122+pandadefi@users.noreply.github.com>
2021-07-14 16:04:37 +0200

    Revert "feat: add health check (#397)"
    This reverts commit 7c5755255b1c17c754113a7bc1e1ba9770997af3.

--- contracts/BaseStrategy.sol
+++ contracts/BaseStrategy.sol
@@ -163,16 +163,6 @@ interface StrategyAPI {
     event Harvested(uint256 profit, uint256 loss, uint256 debtPayment, uint256 debtOutstanding);
 }
 
-interface HealthCheck {
-    function check(
-        uint256 profit,
-        uint256 loss,
-        uint256 debtPayment,
-        uint256 debtOutstanding,
-        uint256 totalDebt
-    ) external view returns (bool);
-}
-
 /**
  * @title Yearn Base Strategy
  * @author yearn.finance
@@ -193,10 +183,6 @@ interface HealthCheck {
 abstract contract BaseStrategy {
     using SafeMath for uint256;
     string public metadataURI;
-
-    // health checks
-    bool public doHealthCheck;
-    address public healthCheck;
 
     /**
      * @notice
@@ -312,11 +298,6 @@ abstract contract BaseStrategy {
         );
     }
 
-    modifier onlyVaultManagers() {
-        require(msg.sender == vault.management() || msg.sender == governance(), "!authorized");
-        _;
-    }
-
     constructor(address _vault) public {
         _initialize(_vault, msg.sender, msg.sender, msg.sender);
     }
@@ -355,14 +336,6 @@ abstract contract BaseStrategy {
         debtThreshold = 0;
 
         vault.approve(rewards, uint256(-1)); // Allow rewards to be pulled
-    }
-
-    function setHealthCheck(address _healthCheck) external onlyVaultManagers {
-        healthCheck = _healthCheck;
-    }
-
-    function setDoHealthCheck(bool _doHealthCheck) external onlyVaultManagers {
-        doHealthCheck = _doHealthCheck;
     }
 
     /**
@@ -775,18 +748,10 @@ abstract contract BaseStrategy {
         // Allow Vault to take up to the "harvested" balance of this contract,
         // which is the amount it has earned since the last time it reported to
         // the Vault.
-        uint256 totalDebt = vault.strategies(address(this)).totalDebt;
         debtOutstanding = vault.report(profit, loss, debtPayment);
 
         // Check if free returns are left, and re-invest them
         adjustPosition(debtOutstanding);
-
-        // call healthCheck contract
-        if (doHealthCheck && healthCheck != address(0)) {
-            require(HealthCheck(healthCheck).check(profit, loss, debtPayment, debtOutstanding, totalDebt), "!healthcheck");
-        } else {
-            doHealthCheck = true;
-        }
 
         emit Harvested(profit, loss, debtPayment, debtOutstanding);
     }
