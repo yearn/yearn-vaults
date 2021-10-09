2edb4332ef4bd41c8d5f43a5bc708e5bd4f7e54a
sam bacha <sam@manifoldfinance.com>
2021-10-08 17:23:12 -0700

    fix(workflow): syntax defect

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,7 @@
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 import { HardhatUserConfig } from "hardhat/types";
-
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
 
 const config: HardhatUserConfig = {
     solidity: {
