b7c1e27b10bb8fbb9bdb7c72b708272804a9809b
panda <87183122+pandadefi@users.noreply.github.com>
2021-10-19 11:06:01 +0200

    chore: remove already imported interface

--- contracts/BaseWrapper.sol
+++ contracts/BaseWrapper.sol
@@ -9,16 +9,6 @@ import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {BaseRouter} from "./BaseRouter.sol";
 import {VaultAPI} from "./BaseStrategy.sol";
 
-interface RegistryAPI {
-    function governance() external view returns (address);
-
-    function latestVault(address token) external view returns (address);
-
-    function numVaults(address token) external view returns (uint256);
-
-    function vaults(address token, uint256 deploymentId) external view returns (address);
-}
-
 /**
  * @title Yearn Base Wrapper
  * @author yearn.finance
