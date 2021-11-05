5f4476f596868bbcc9691cf1234f524d026ae7cf
sam bacha <sam@manifoldfinance.com>
2021-11-05 15:49:31 -0700

    ci(commit): fix commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
 /**
-* Hardhat Configuration
-* @module hardhat
-*/
+ * Hardhat Configuration
+ * @module hardhat
+ */
 module.exports = {
-solidity: {
-    version: "0.6.12",
-    settings: {
-        metadata: {
-            bytecodeHash: "none",
-        },
-        optimizer: {
-            enabled: false,
-            runs: 200,
-            details: {
-                yul: false,
+    solidity: {
+        version: "0.6.12",
+        settings: {
+            metadata: {
+                bytecodeHash: "none",
+            },
+            optimizer: {
+                enabled: false,
+                runs: 200,
+                details: {
+                    yul: false,
+                },
             },
         },
     },
-  },
-}
+};
