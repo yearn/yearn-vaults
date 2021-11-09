a7dc0d3591cb3d0aaa47f858959cda91b4bf3b10
Rare Weasel <therareweasel@gmail.com>
2021-11-09 15:30:41 -0600

    chore: fix ci cd issues

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -88,9 +88,11 @@ def strategist(accounts):
 def keeper(accounts):
     yield accounts[5]
 
+
 @pytest.fixture(autouse=True)
 def StrategyLib(strategist, StrategyLib):
     yield strategist.deploy(StrategyLib)
+
 
 @pytest.fixture(params=["RegularStrategy", "ClonedStrategy"])
 def strategy(gov, strategist, keeper, rewards, vault, TestStrategy, request):
