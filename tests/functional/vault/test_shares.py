6c5c87a7b1818a5c37a4f701dc77af3ccf7ed84d
pandadefi <87183122+pandadefi@users.noreply.github.com>
2021-11-30 18:28:40 +0100

    test: add function to increase pps with a strategy (#492)
    * test: add function to increase pps with a strategy
    
    * fix: use a function instead of a fixture
    
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
    
    * fix: get strategy from fixture
    
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- tests/functional/vault/test_shares.py
+++ tests/functional/vault/test_shares.py
@@ -269,10 +269,10 @@ def test_transferFrom(accounts, token, vault):
     assert vault.balanceOf(b) == token.balanceOf(vault)
 
 
-def test_do_not_issue_zero_shares(gov, token, vault):
+def test_do_not_issue_zero_shares(gov, token, vault, increase_pps):
     token.approve(vault, 500, {"from": gov})
     vault.deposit(500, {"from": gov})
-    token.transfer(vault, 500)  # inflate price
+    strategy = increase_pps(vault, token, 500, gov)  # inflate price
     assert vault.pricePerShare() == 2 * 10 ** token.decimals()  # 2:1 price
     with brownie.reverts():
         vault.deposit(1, {"from": gov})
